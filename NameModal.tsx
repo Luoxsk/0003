fe2e3f90360181566430cc6802829fd1f8daa709
Luoxsk <2993162631@qq.com>
2025-11-24 18:04:04 +0800

    Update import paths in NameModal.tsx

--- NameModal.tsx
+++ NameModal.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
-import { UI_TEXT, PET_OPTIONS } from '../constants';
-import { PetType } from '../types';
+import { UI_TEXT, PET_OPTIONS } from './constants';
+import { PetType } from './types';
 
 interface NameModalProps {
   isOpen: boolean;
