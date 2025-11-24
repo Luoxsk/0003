ccf175147a097874aa689f951e7ed3cd6bd0eaac
Luoxsk <2993162631@qq.com>
2025-11-24 18:02:11 +0800

    Update import path for types in DailyCheckIn.tsx

--- DailyCheckIn.tsx
+++ DailyCheckIn.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { MoodType, DailyEntry, MoodConfig } from '../types';
+import { MoodType, DailyEntry, MoodConfig } from './types';
 import { MOOD_OPTIONS, UI_TEXT } from './constants';
 import { Axe } from 'lucide-react';
 
